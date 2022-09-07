c960901b50643568f9a33fcc7c0cb6060f351bda
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2022-09-07 20:39:18 +0300

    del backend folder

--- backend/routers/message.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const { deleteMessage } = require('../controllers/message')
-
-router.post('/delete', deleteMessage)
-
-module.exports = router
